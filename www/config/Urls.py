2dded76aafa7f1e34bf70d352352a16e5b70bc22
Coral Young <coralyoung@outlook.com>
2012-09-03 18:43:04 +0800

    rename Urls.py

---
+++ www/config/Urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+prefix = 'controllers.'
+
+urls = (
+		"index", prefix + "CIndex.Index"
+		"about", prefix + "CAbout.About"
+		)
--- www/config/urls.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-
-urls = (
-		"index", "CIndex.Index"
-		"about", "CAbout.About"
-		)
