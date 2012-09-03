e68fc3ec167443c139124228e8e20b2f8eba946b
Coral Young <coralyoung@outlook.com>
2012-09-03 17:44:29 +0800

    modiry index.py

--- www/index.py
+++ www/index.py
@@ -10,6 +10,6 @@ class hello:
 	def GET(self):
 		return 'Hello, world!'
 
-	if __name__ == "__main__":
-		web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
-		app.run()
+if __name__ == "__main__":
+	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+	app.run()
